d5dea778e8d8f8b8aa44e9c4fa33ee8658a2a86e
ASUS <ASUS@LAPTOP-IS6K6VA3>
2023-11-21 12:12:41 +0700

    taufik

---
+++ src/main/java/com/mycompany/perbaikan_uts/Perbaikan_uts.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.perbaikan_uts;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Perbaikan_uts {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
